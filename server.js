500b4ef4fd2115a2baf90fb7dd0f3eefcc659075
Cody Thaller <stevencthaller@gmail.com>
2020-09-02 15:25:18 -0700

    It works!

--- server.js
+++ server.js
@@ -7,7 +7,16 @@ client.login(config.BOT_TOKEN);
 client.on('voiceStateUpdate', (oldMember, newMember) => {
     let channel = newMember.guild.channels.cache.get(newMember.channelID);
 
-    if(channel && channel.name.includes('➕') && channel.name.includes('Session')){
-        if(channel.parentID == '750798247429275718');
+    if(channel && channel.name.includes('➕') && channel.name.includes('help')){
+        let user =channel.guild.members.cache.get(newMember.id);
+        client.channels.cache.get('738524948431241217').send(`@here ${user.displayName} needs help!`);
     }
 })
+// client.on('voiceStateUpdate', (oldMember, newMember) => {
+//     let channel = newMember.guild.channels.cache.get(newMember.channelID);
+
+//     if(channel && channel.name.includes('➕') && channel.name.includes('help')){
+//         let user =channel.guild.members.cache.get(newMember.id);
+//         client.channels.cache.get('750792764844802048').send(`@here ${user.displayName} needs help!`);
+//     }
+// })
