fc644bf50118a3768feaf1b29a9413f15b788133
Cody Thaller <stevencthaller@gmail.com>
2020-09-02 14:17:51 -0700

    First commit for discord bot

---
+++ server.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const config = require('./config.json');
+
+const client = new Discord.Client();
+client.login(config.BOT_TOKEN);
+
+client.on('voiceStateUpdate', (oldMember, newMember) => {
+    let channel = newMember.guild.channels.cache.get(newMember.channelID);
+
+    if(channel && channel.name.includes('➕') && channel.name.includes('Session')){
+        if(channel.parentID == '750798247429275718');
+    }
+})
